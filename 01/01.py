ec0466fd1ce1c6740fd90ea3526e587d79732217
Mikuláš Zelinka <miki.myke@gmail.com>
2018-12-21 20:49:51 +0100

    day 01 and 02: close files earlier

--- 01/01.py
+++ 01/01.py
@@ -1,18 +1,18 @@
 with open('input.txt') as f:
     freqs = [int(x) for x in f.readlines()]
 
-    # part 1:
-    print(f'1: {sum(freqs)}')
+# part 1:
+print(f'1: {sum(freqs)}')
 
-    # part 2:
-    result_freq = 0
-    found = {result_freq}
-    finished = False
-    while not finished:
-        for x in freqs:
-            result_freq += x
-            if result_freq in found:
-                print(f'2: {result_freq}')
-                finished = True
-                break
-            found.add(result_freq)
+# part 2:
+result_freq = 0
+found = {result_freq}
+finished = False
+while not finished:
+    for x in freqs:
+        result_freq += x
+        if result_freq in found:
+            print(f'2: {result_freq}')
+            finished = True
+            break
+        found.add(result_freq)
